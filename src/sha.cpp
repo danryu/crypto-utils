9b9441a07a581f4f2e61aab7dff9e3c1eb2a49cd
danryu <dan.garton@gmail.com>
2026-05-24 11:38:17 +0200

    Add MSVC build support (Windows / cl.exe)

--- src/sha.cpp
+++ src/sha.cpp
@@ -11,25 +11,23 @@ declare_autoptr(MDContext, EVP_MD_CTX, EVP_MD_CTX_free);
 template <size_t N>
 auto calc_generic(const BytesRef data, const char* const algo) -> std::optional<std::array<std::byte, N>> {
     auto ctx = AutoMDContext(EVP_MD_CTX_new());
-    ensure(ctx.get() != NULL);
+    if(ctx.get() == NULL) return std::nullopt;
     auto md = EVP_get_digestbyname(algo);
-    ensure(md != NULL);
+    if(md == NULL) return std::nullopt;
 
     auto buf = std::array<std::byte, N>();
-    ensure(EVP_DigestInit_ex(ctx.get(), md, NULL) == 1);
-    ensure(EVP_DigestUpdate(ctx.get(), data.data(), data.size()) == 1);
-    ensure(EVP_DigestFinal_ex(ctx.get(), reinterpret_cast<unsigned char*>(buf.data()), NULL) == 1);
+    if(EVP_DigestInit_ex(ctx.get(), md, NULL) != 1) return std::nullopt;
+    if(EVP_DigestUpdate(ctx.get(), data.data(), data.size()) != 1) return std::nullopt;
+    if(EVP_DigestFinal_ex(ctx.get(), reinterpret_cast<unsigned char*>(buf.data()), NULL) != 1) return std::nullopt;
     return buf;
 }
 } // namespace
 
 auto calc_sha1(const BytesRef data) -> std::optional<std::array<std::byte, 20>> {
-    unwrap(ret, calc_generic<20>(data, "SHA1"));
-    return ret;
+    return calc_generic<20>(data, "SHA1");
 }
 
 auto calc_sha256(const BytesRef data) -> std::optional<std::array<std::byte, 32>> {
-    unwrap(ret, calc_generic<32>(data, "SHA256"));
-    return ret;
+    return calc_generic<32>(data, "SHA256");
 }
 } // namespace crypto::sha
